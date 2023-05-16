8d5ca3a2c32572f91015976239f888d362b9f9e4
Henry17C <hicoyago@uce.edu.ec>
2023-05-15 19:35:35 -0500

    Taller 4: creacion porfesor

--- src/main/java/com/example/demo/Pa2U1P4HcJtApplication.java
+++ src/main/java/com/example/demo/Pa2U1P4HcJtApplication.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,6 +23,23 @@ public class Pa2U1P4HcJtApplication implements CommandLineRunner{
 		 *  y desarrollo de aplcaciones empresariales 
 		 *  basadas en Spring
 		 */
+		
+		Profesor profesor= new Profesor();
+		profesor.setApellido("Coyago");
+		profesor.setCedula("17555");
+		profesor.setFechaNacimiento(LocalDateTime.now());;
+		profesor.setNombre("Henry");
+		
+		
+		System.out.println(profesor);
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
